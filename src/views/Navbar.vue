eb760605dba18b56b8d9fde2a9c1f2ec6fe3d669
DESKTOP-RAE7E6C\User <abbywupp@gmail.com>
2024-05-27 15:23:55 +0800

    NavBar修改

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -1,96 +1,121 @@
 <template>
-    <div style="display: flex;">
-        <RouterLink to="/" class="dropdown-item" style="width: 20%;">
-            <img src="/logo.jpeg" style="width: 100%;">
-        </RouterLink>
-        <div style="width: 25%; margin-left: 20%; margin-top: 4%;">
-<!--            <Search @searchcons="dosearch"></Search>-->
-        </div>
-        
+  <div style="display: flex;">
+    <RouterLink to="/" class="dropdown-item" style="width: 15%;">
+      <img src="/logo.jpeg" style="width: 100%;">
+    </RouterLink>
+    <div style="width: 25%; margin-left: 20%; margin-top: 4%;">
+      <Search @searchcons="dosearch"></Search>
     </div>
-    
-    <!-- <nav class="navbar navbar-expand-lg" style="background-color: white"> -->
-    <nav class="navbar navbar-expand-lg" style="background-color: #D1E9E9;">
 
-        <div class="container-fluid">
-            <!-- <a class="navbar-brand" href="#">CoChordNuts</a> -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span   span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <!--Product -->
-                        <RouterLink class="nav-link dropdown-toggle" to="/products" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        全部分類
-                        </RouterLink>
-                        <ul class="dropdown-menu">
-                            <li><RouterLink to="/product/cn" class="dropdown-item">華語</RouterLink></li>
-                            <li><RouterLink to="/product/JpKr" class="dropdown-item">日韓</RouterLink></li>
-                            <li><RouterLink to="/product/en" class="dropdown-item">西洋</RouterLink></li>
-                            <li><RouterLink to="/products" class="dropdown-item">全部商品</RouterLink></li>
-                            <li><RouterLink to="/product/isBest" class="dropdown-item">熱銷商品</RouterLink></li>
-                            <li><RouterLink to="/product/isDiscount" class="dropdown-item">折扣商品</RouterLink></li>
-                            <li><RouterLink to="/product/isPreorder" class="dropdown-item">預購商品</RouterLink></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        會員中心
-                      </a>
-                      <ul class="dropdown-menu">
-                        <li><RouterLink to="/member" class="dropdown-item">會員基本資料</RouterLink></li>
-                        <li><RouterLink to="/modify" class="dropdown-item">修改會員資料</RouterLink></li>
-                        <li><RouterLink to="/modifyPwd" class="dropdown-item">修改密碼</RouterLink></li>
-                        <li><RouterLink to="/delete" class="dropdown-item">刪除帳號</RouterLink></li>
-                        <li><RouterLink to="/order/userFindAllOrders" class="dropdown-item">使用者訂單紀錄</RouterLink></li>
-                      </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <!--Product -->
-                      <RouterLink class="nav-link dropdown-toggle" to="/products" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        客戶服務
-                      </RouterLink>
-                      <ul class="dropdown-menu">
-                        <li>
-                          <RouterLink class="dropdown-item" to="/Customer/CustomerAnswer">客服提問</RouterLink>
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="nav-item">
-                      <RouterLink class="nav-link active" aria-current="page" to="/cart">購物車</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                      <RouterLink class="nav-link active" aria-current="page" :to="loginOrLogoutLink">
-                        {{ loginOrLogoutText }}
-                      </RouterLink>
-                    </li>
-                    <li class="nav-item" v-if="!isLoggedIn">
-                      <RouterLink class="nav-link active" aria-current="page" to="/register">註冊</RouterLink>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-        
+  </div>
+
+  <!-- <nav class="navbar navbar-expand-lg" style="background-color: white"> -->
+  <nav class="navbar navbar-expand-lg" style="background-color: #D1E9E9;">
+
+    <div class="container-fluid">
+      <!-- <a class="navbar-brand" href="#">CoChordNuts</a> -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <!--Product -->
+            <RouterLink class="nav-link dropdown-toggle" to="/products" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              全部分類
+            </RouterLink>
+            <ul class="dropdown-menu">
+              <li>
+                <RouterLink to="/product/cn" class="dropdown-item">華語</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/product/JpKr" class="dropdown-item">日韓</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/product/en" class="dropdown-item">西洋</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/products" class="dropdown-item">全部商品</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/product/isBest" class="dropdown-item">熱銷商品</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/product/isDiscount" class="dropdown-item">折扣商品</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/product/isPreorder" class="dropdown-item">預購商品</RouterLink>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              會員中心
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <RouterLink to="/member" class="dropdown-item">會員基本資料</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/modify" class="dropdown-item">修改會員資料</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/modifyPwd" class="dropdown-item">修改密碼</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/delete" class="dropdown-item">刪除帳號</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/order/userFindAllOrders" class="dropdown-item">使用者訂單紀錄</RouterLink>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <!--Product -->
+            <RouterLink class="nav-link dropdown-toggle" to="/products" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              客戶服務
+            </RouterLink>
+            <ul class="dropdown-menu">
+              <li>
+                <RouterLink class="dropdown-item" to="/Customer/CustomerAnswer">客服提問</RouterLink>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link active" aria-current="page" to="/cart">購物車</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link active" aria-current="page" :to="loginOrLogoutLink">
+              {{ loginOrLogoutText }}
+            </RouterLink>
+          </li>
+          <li class="nav-item" v-if="!isLoggedIn">
+            <RouterLink class="nav-link active" aria-current="page" to="/register">註冊</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
 </template>
-    
+
 <script setup>
-    import { ref } from 'vue';
-    import Search from '@/components/Search.vue';
-    import { computed } from 'vue';
+import { ref } from 'vue';
+import Search from '@/components/Search.vue';
+import { computed } from 'vue';
 
-    const props = defineProps({isLoggedIn: Boolean});
+const props = defineProps({ isLoggedIn: Boolean });
 
-    const loginOrLogoutText = computed(() => props.isLoggedIn ? '登出' : '登入');
-    const loginOrLogoutLink = computed(() => props.isLoggedIn ? '/secure/logout' : '/secure/login');
+const loginOrLogoutText = computed(() => props.isLoggedIn ? '登出' : '登入');
+const loginOrLogoutLink = computed(() => props.isLoggedIn ? '/secure/logout' : '/secure/login');
 
-    function dosearch(data){
-        console.log(data);
-    }
+function dosearch(data) {
+  console.log(data);
+}
 
 </script>
-    
-<style>
-    
-</style>
+
+<style></style>
