9621870b5461f44e1f6f3b89e6ceb79ddb054e98
lin888688 <164441308+lin888688@users.noreply.github.com>
2024-05-27 15:35:20 +0800

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from "vue-router";
 //
 // //引入 SFC 元件
 import Home from '../views/Home.vue';
-// import Member from '../views/Member/member-routes.js';
+import Member from '../views/Member/member-routes.js';
 import Product from '../views/Product/product-routes.js';
 // import Order from '../views/Order/order-routes.js';
 // import Customer from '../views/Customer/customer-routes.js';
