1283d7f5689d1ee735f5d6617ca66211cf235598
Abby <abbywupp@gmail.com>
2024-05-28 23:02:13 +0800

    update ecpay

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router.js';
+// import ecpay from '@/router/index.js';
 
 createApp(App)
     .use(router)
+    // .use(ecpay)
     .mount('#app')
