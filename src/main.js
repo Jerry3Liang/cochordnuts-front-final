e053153010392d10495ea5f681c18c062af265f7
DESKTOP-RAE7E6C\User <abbywupp@gmail.com>
2024-05-29 14:20:19 +0800

    ecpay修改

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router.js';
-// import ecpay from '@/router/index.js';
 
 createApp(App)
     .use(router)
-    // .use(ecpay)
     .mount('#app')
