6026884b36af058a7f6d4cc0a21acdbdf5bcddf3
DESKTOP-RAE7E6C\User <mini3924@gmail.com>
2024-05-30 15:42:25 +0800

    修改footer

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,11 +1,13 @@
 <template>
-    <div style="background-color: #CCCCCC; margin-top: 5%;">
-        <h3>Footer</h3>
-        <p>佳群唱片有限公司   統編：16611935   台北市 108 萬華區中華路一段110號3樓</p>
-        <p>訂單專線：(02) 2370-2793   /   產品專線：(02) 2312-3437   /   傳真專線：(02) 2389-8506   /   客服信箱：service@ccr.com.tw</p>
-        <p>專線服務時間：星期一~星期五上午10:30-下午08:00：星期六~星期日(國定假日) 上午11:00-下午06:00</p>
+    <div style="background-color: #CCCCCC; margin-top: 5%; text-align: center; padding: 1%;" >
+        <h3>CoChordNuts唱片行</h3>
+        <p>椰子樹唱片有限公司     統編：16611935     106 台北市大安區復興南路一段390號2樓</p>
+        <p>訂單專線：(02) 6631 6588   /   產品專線：(02) 2312-3437   /   傳真專線：(02) 2389-8506   /   客服信箱：service@cochordnut.com.tw</p>
+        <p>專線服務時間：星期一~星期五上午09:00-下午04:30：星期六~星期日(國定假日) 上午11:00-下午06:00</p>
     </div>
-        
+    <div class="container my-5">
+
+</div>
 </template>
     
 <script setup>
