cb784b6ad84266cf31e779de1d96d0dd3bd6512f
lin888688 <164441308+lin888688@users.noreply.github.com>
2024-06-04 02:04:18 +0800

    Update Member.vue

--- src/views/Member/Member.vue
+++ src/views/Member/Member.vue
@@ -1,44 +1,102 @@
 <template>
-    <div class="center" v-if="member">
-        
-    <div class="center-content">
-        <h1>會員基本資料</h1>
-        <ul class="list-group">
-            
-            <li class="list-group-item">
-                <span class="label">姓名:</span><span class="label-in"> {{ member.name }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">生日:</span> <span class="label-in">{{ formatBirthday(member.birthday) }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">註冊日:</span><span class="label-in">{{ member.registerTime }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">上次登入日:</span> <span class="label-in">{{ member.lastLoginTime }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">Email:</span><span class="label-in">{{ member.email }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">地址:</span><span class="label-in">{{ member.address }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">電話:</span><span class="label-in">{{ member.phone }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">常用收件人:</span><span class="label-in"> {{ member.recipient }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">常用收件人地址:</span> <span class="label-in">{{ member.recipientAddress }}</span>
-            </li>
-            <li class="list-group-item">
-                <span class="label">常用收件人電話:</span><span class="label-in"> {{ member.recipientPhone }}</span>
-            </li>
-        </ul>
+    <div class="container" v-if="member">
+
+        <div class="col-md-9" style="margin: auto;">
+            <div class="card">
+                <div class="card-body">
+
+                    <div class="col-md-12">
+                        <h3>會員資料</h3>
+                        <hr>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <form>
+                                <div class="form-group row">
+                                    <label for="name" class="col-4 col-form-label">姓名:</label>
+                                    <div class="col-8">
+                                        <input id="name" name="name" placeholder="姓名" class="form-control here"
+                                            required="required" type="text" v-model="member.name" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="birthday" class="col-4 col-form-label">生日:</label>
+                                    <div class="col-8">
+                                        <input id="birthday" name="birthday" placeholder="生日" class="form-control here"
+                                            type="text" :value="formatBirthday(member.birthday)" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="registerTime" class="col-4 col-form-label">註冊日:</label>
+                                    <div class="col-8">
+                                        <input id="registerTime" name="registerTime" placeholder="註冊日"
+                                            class="form-control here" type="text" :value="member.registerTime" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="lastLoginTime" class="col-4 col-form-label">上次登入日:</label>
+                                    <div class="col-8">
+                                        <input id="lastLoginTime" name="lastLoginTime" placeholder="上次登入日"
+                                            class="form-control here" type="text" :value="member.lastLoginTime"
+                                            disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="email" class="col-4 col-form-label">Email:</label>
+                                    <div class="col-8">
+                                        <input id="email" name="email" placeholder="Email" class="form-control here"
+                                            type="text" v-model="member.email" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="address" class="col-4 col-form-label">地址:</label>
+                                    <div class="col-8">
+                                        <input id="address" name="address" placeholder="地址" class="form-control here"
+                                            type="text" v-model="member.address" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="phone" class="col-4 col-form-label">電話:</label>
+                                    <div class="col-8">
+                                        <input id="phone" name="phone" placeholder="電話" class="form-control here"
+                                            type="text" v-model="member.phone" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="recipient" class="col-4 col-form-label">常用收件人:</label>
+                                    <div class="col-8">
+                                        <input id="recipient" name="recipient" placeholder="常用收件人"
+                                            class="form-control here" type="text" v-model="member.recipient" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="recipientAddress" class="col-4 col-form-label">常用收件人地址:</label>
+                                    <div class="col-8">
+                                        <input id="recipientAddress" name="recipientAddress" placeholder="常用收件人地址"
+                                            class="form-control here" type="text" v-model="member.recipientAddress"
+                                            disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="recipientPhone" class="col-4 col-form-label">常用收件人電話:</label>
+                                    <div class="col-8">
+                                        <input id="recipientPhone" name="recipientPhone" placeholder="常用收件人電話"
+                                            class="form-control here" type="text" v-model="member.recipientPhone"
+                                            disabled>
+                                    </div>
+                                </div>
+                            </form>
+                            <RouterLink to="/memberModify">
+                                <button class="btn btn-primary" type="button">修改</button>
+                            </RouterLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-    <button class="btn btn-primary" type="button" @click="newPage()">修改</button>
-</div>
 </template>
 
 <script setup>
@@ -52,23 +110,26 @@ const member = ref(null);
 
 onMounted(() => {
     const loggedInMemberNo = sessionStorage.getItem("memberNo");
-    if(loggedInMemberNo !=null){
-    axiosapi.get(`/members/${loggedInMemberNo}`)
-        .then(response => {
-            member.value = response.data.list[0];
-        })}else{ Swal.fire({
-                        text: '尚未登入',
-                        icon: 'error',
-                        allowOutsideClick: false,
-                        confirmButtonText: '確認',
-                }); 
-                    router.push({ name: "login-link" });
-                }
+    if (loggedInMemberNo != null) {
+        axiosapi.get(`/members/${loggedInMemberNo}`)
+            .then(response => {
+                member.value = response.data.list[0];
+            })
+            .catch(error => {
+                console.error('Error fetching member data:', error);
+            });
+    } else {
+        Swal.fire({
+            text: '尚未登入',
+            icon: 'error',
+            allowOutsideClick: false,
+            confirmButtonText: '確認',
+        }).then(() => {
+            router.push({ name: "login-link" });
+        });
+    }
 });
 
-function newPage(){
-    router.push({ name: "memberModify-link" });
-}
 
 const formatBirthday = (dateString) => {
     if (dateString.includes('T')) {
@@ -79,37 +140,40 @@ const formatBirthday = (dateString) => {
 };
 </script>
 
-<style>
-.center-content{
-    max-width: 100%;  
+<style scoped>
+.container {
+
+    margin: auto;
+
+    max-width: 80%;
+}
+
+.card {
     margin-top: 40px;
 }
 
-.center {
-    justify-content: center;
-    width: 900px;
+
+.form-group.row {
+    margin-bottom: 15px;
 }
 
-h1{
-    font-size: 40px
-}
-
-.label {
-    font-weight: 900;
-    font-size: larger;
-    margin-left: 0;
-    padding-bottom: 0;
-    color: #494949;
-}
-
-.label-in{
+label {
     font-size: 18px;
-    margin-left: 150px;
-    border-radius: 5px;
+    font-weight: 550;
 }
-.list-group {
+
+.col-4 {
     display: flex;
-    margin-bottom: 5px;
-    width: 80%;
+    align-items: center;
+    padding-right: 5px;
+}
+
+.col-8 {
+    padding-left: 5px;
+}
+
+.btn-primary {
+    margin-top: 20px;
+    margin-left: 45%;
 }
 </style>
