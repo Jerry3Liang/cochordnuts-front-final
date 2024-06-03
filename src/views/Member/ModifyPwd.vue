0d91e2aace26e91d0764a45a806c29e00f13c0ab
lin888688 <164441308+lin888688@users.noreply.github.com>
2024-06-04 02:05:08 +0800

    Update ModifyPwd.vue

--- src/views/Member/ModifyPwd.vue
+++ src/views/Member/ModifyPwd.vue
@@ -1,16 +1,14 @@
 <template>
     <div class="center">
         <div class="center-content">
-            <h1>修改密碼</h1>
+            <h3 style="margin-top: 5%">修改密碼</h3>
             <ul class="list-group">
-                
-                <li class="list-group-item">
-                    <span class="label">原密碼:</span>
-                    
-                    <input type="password" class="label-modify" v-model="member.oriPassword" id="myInput"
-                        required>
-                        
-                </li>
+
+                <label class="col-4 col-form-label">原密碼:</label>
+                <div class="col-8">
+                    <input id="myInput" class="form-control here" type="password" v-model="member.oriPassword" required>
+                </div>
+
                 <div class="form-check text-start my-3">
                     <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"
                         @click="myFunction()">
@@ -18,13 +16,12 @@
                         顯示密碼
                     </label>
                 </div>
-                <li class="list-group-item">
-                    <span class="label">新密碼:</span> 
-                    
-                    <input type="password" class="label-modify" v-model="member.password" id="myInput1"
-                        required >
 
-                </li>
+                <label class="col-4 col-form-label">新密碼:</label>
+                <div class="col-8">
+                    <input id="myInput1" class="form-control here" type="password" v-model="member.password" required>
+                </div>
+
                 <div class="form-check text-start my-3">
                     <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault1"
                         @click="myFunction1()">
@@ -32,13 +29,12 @@
                         顯示密碼
                     </label>
                 </div>
-                <li class="list-group-item">
-                    <span class="label">再輸入一次新密碼:</span> 
-                    
-                    <input type="password" class="label-modify" v-model="member.confirmPassword" id="myInput2"
-                        required >
 
-                </li>
+                <label class="col-4 col-form-label">再輸入一次新密碼:</label>
+                <div class="col-8">
+                    <input id="myInput2" class="form-control here" type="password" v-model="member.confirmPassword"
+                        required>
+                </div>
                 <div class="form-check text-start my-3">
                     <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault2"
                         @click="myFunction2()">
@@ -68,7 +64,7 @@ const router = useRouter();
 const member = ref({
     oriPassword: '',
     password: '',
-    confirmPassword:''
+    confirmPassword: ''
 })
 
 function myFunction() {
@@ -97,22 +93,24 @@ function myFunction2() {
 }
 onMounted(() => {
     const memberNo = sessionStorage.getItem("memberNo");
-    if(memberNo !=null){
-    axiosapi.get(`/members/${memberNo}`)
-        .then(response => {
-            member.value = response.data.list[0];
-        })}else{ Swal.fire({
-                        text: '尚未登入',
-                        icon: 'error',
-                        allowOutsideClick: false,
-                        confirmButtonText: '確認',
-                }); 
-                    router.push({ name: "login-link" });
-                }
+    if (memberNo != null) {
+        axiosapi.get(`/members/${memberNo}`)
+            .then(response => {
+                member.value = response.data.list[0];
+            })
+    } else {
+        Swal.fire({
+            text: '尚未登入',
+            icon: 'error',
+            allowOutsideClick: false,
+            confirmButtonText: '確認',
+        });
+        router.push({ name: "login-link" });
+    }
 });
 
 
-function callModifyPwd(){
+function callModifyPwd() {
     if (member.value.password != member.value.confirmPassword) {
         Swal.fire({
             text: '密碼不一致，請重新輸入。',
@@ -120,9 +118,8 @@ function callModifyPwd(){
             allowOutsideClick: false,
             confirmButtonText: '確認',
         });
-    }else{
+    } else {
         Swal.fire({
-
             title: "確認修改?",
             icon: "warning",
             showCancelButton: true,
@@ -133,39 +130,50 @@ function callModifyPwd(){
             if (result.isConfirmed) {
                 let memberNo = sessionStorage.getItem("memberNo");
                 axiosapi.put(`/member/changepwd/${memberNo}`, member.value)
-                .then(function (response) {
-                    if (response.data.success) {
+                    .then(function (response) {
+                        if (response.data.success) {
+                            Swal.fire({
+                                title: "修改成功!",
+                                icon: "success"
+                            }).then(function () {
+                                router.push({ name: "member-link" });
+                            });
+                        } else {
+                            Swal.fire({
+                                text: response.data.message || '修改失敗!',
+                                icon: 'error',
+                                allowOutsideClick: false,
+                                confirmButtonText: '確認',
+                            }); router.push({ name: "modifyPwd-link" });
+                        }
+                    })
+                    .catch(function (error) {
                         Swal.fire({
-                            title: "修改成功!",
-                            icon: "success"
-                        }).then(function () {
-                            router.push({ name: "member-link" });
-                        });
-                    } else {
-                        Swal.fire({
-                            text: response.data.message || '修改失敗!',
+                            text: '修改失敗：' + error.message,
                             icon: 'error',
                             allowOutsideClick: false,
                             confirmButtonText: '確認',
-                        }); router.push({ name: "modifyPwd-link" });
-                    }
-                })
-                .catch(function (error) {
-                    Swal.fire({
-                        text: '修改失敗：' + error.message,
-                        icon: 'error',
-                        allowOutsideClick: false,
-                        confirmButtonText: '確認',
+                        });
                     });
-                });
-        } else {
-            router.push({ name: "member-link" });
-        };
-    });}
+            } else {
+                router.push({ name: "member-link" });
+            };
+        });
+    }
 }
 
 </script>
-    
+
 <style>
-    
+.center {
+
+    margin: auto;
+
+    max-width: 80%;
+}
+
+.col-4 {
+    font-size: 18px;
+    font-weight: 550;
+}
 </style>
