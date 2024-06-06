20a4ceed651743f98a66f3157ff8ac02ee3e3083
Abby <abbywupp@gmail.com>
2024-06-06 19:39:50 +0800

    https設定

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  devServer: {
-    public: 'http://192.168.31.96:8080/'
-  },
+  // devServer: {
+  //   public: 'http://192.168.31.96:8080/'
+  // },
   plugins: [
     vue(),
+    mkcert()
   ],
+  server: {
+    https: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
